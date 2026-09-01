159d3572878c20de3d2bc6341c9a2c9f0fdf68be
priyam63p <priyampatra63@gmail.com>
2026-09-01 20:12:10 +0530

    Add Lec_2 folder

---
+++ Lec_2/script.js
@@ -0,0 +1,61 @@
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360
+// })
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     // scrollTrigger:"#page2 #box"
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:"true",
+
+//     }
+// })
+
+// gsap.from("#page2 h1", {
+//   opacity: 0,
+//   duration: 2,
+//   x: 500,
+//   scrollTrigger: {
+//     trigger: "#page2 h1",
+//     scroller: "body",
+//     markers: true,
+//   },
+// });
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     opacity:0,
+//     rotate:720,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%",
+//         end:"top 30%",
+//         scrub:2,
+//         pin:true
+//     }
+// })
+
+gsap.to("#page2 h1", {
+  transform: "translateX(-150%)",
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller: "body",
+
+    start: "top 0%",
+    markers: true,
+    end: "top 100%",
+    scrub: 2,
+    pin: true,
+  },
+});
